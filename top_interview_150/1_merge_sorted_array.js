313c708bf2b91d09dd658ff93b32aec62d94ba52
Muhammad Saeed <mouhammedsaed@gmail.com>
2023-09-13 23:46:31 +0300

    solveing the same problem in a second way using the two pointer technique

--- top_interview_150/1_merge_sorted_array.js
+++ top_interview_150/1_merge_sorted_array.js
@@ -2,6 +2,8 @@
 let nums1 = [1, 2, 3, 0, 0, 0];
 let nums2 = [2, 5, 6];
 
+
+// using the simpler way STL: Standard Template Library
 function merge(arr1, m, arr2, n) {
    for (let i = m, j = 0; j < n; i++, j++) {
     arr1[i] = arr2[j];
@@ -9,7 +11,18 @@ function merge(arr1, m, arr2, n) {
    nums1.sort((a, b) => a - b);
 }
 
+// using the two pointer technique
+function merge1(arr1, m, arr2, n) {
+   let i = m - 1;
+   let j = n -1;
+   let k = m + n - 1;
 
-merge(nums1, 3, nums2, nums2.length);
+   while(j >= 0) {
+      if ( i >= 0 && arr1[i] > arr2[j]) arr1[k--] = arr1[i--];
+      else arr1[k--] = arr2[j--];
+   }
+}
+
+merge1(nums1, 3, nums2, nums2.length);
 console.log(nums1);
 console.log(nums2);
